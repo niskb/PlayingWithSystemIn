2e7af270cd8a9fa66304ddcf5254d67e7d8e1c83
Brian Niski <niskb19@gmail.com>
2022-10-14 15:53:47 -0400

    Print System Properties
    Prints some System properties

--- src/p1/Demo.java
+++ src/p1/Demo.java
@@ -8,6 +8,11 @@ public class Demo {
 	
 	private static void printSystemInformation() {
 		System.out.println("System Information:");
+		System.out.println("");
+		System.out.println("OS Architecture: " + System.getProperty("os.arch"));
+		System.out.println("OS Name: " + System.getProperty("os.name"));
+		System.out.println("File Seperator: " + System.getProperty("file.separator"));
+		System.out.println("User Account Name: " + System.getProperty("user.name"));
 	}
 	
 }
