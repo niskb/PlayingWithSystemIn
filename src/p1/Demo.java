fd0bc505efef2b13f589bd090dc8b2a2c04b5340
Brian Niski <niskb19@gmail.com>
2022-10-14 15:35:34 -0400

    Add printSystemInformation Method
    This method is used to print some information on the console

--- src/p1/Demo.java
+++ src/p1/Demo.java
@@ -3,7 +3,11 @@ package p1;
 public class Demo {
 
 	public static void main(String[] args) {
-		
+		printSystemInformation();
+	}
+	
+	private static void printSystemInformation() {
+		System.out.println("System Information:");
 	}
 	
 }
